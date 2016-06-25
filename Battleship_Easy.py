015d34c5fa5346a45551882810069abec9b373db
limahseng <lim.ahseng@dhs.sg>
2016-06-25 11:54:04 +0800

    Update Battleship_Easy.py
    fixed final reveal position +1 display

--- Battleship_Easy.py
+++ Battleship_Easy.py
@@ -1,45 +1,61 @@
-from random import randint  # randint generates a random interger in the range provided
+from random import randint  # randint generates a random integer in the range provided
 
-def print_board(board): 
+# print board row by row
+def print_board(board):
     for row in board:
-        print (" ".join(row)) # prints the board row by row in a legible format
+        print (" ".join(row)) 
 
+# generate random number for board row index
 def random_row(board):
-    return randint(0, len(board) - 1) # generates a random number corresponding to board row index
+    return randint(0, len(board) - 1)
 
+# generate random number for board column index
 def random_col(board):
-    return randint(0, len(board[0]) - 1) # generates a random number corresponding to board column index
-
+    return randint(0, len(board[0]) - 1)
+    
+    
 # main
-board = [] # initializes an empty board so that spaces can be appended to it later
+# initialize empty board
+board = []
 
+# insert 3 rows of ["O","O","O"] into the board
 for x in range(3):
-    board.append(["O"] * 3) # inserts three rows of ["O","O","O"] into the board 
+    board.append(["O"] * 3) 
 
+# show intro message
 print("Let's play Battleship!")
-print_board(board) 
-ship_row = random_row(board) # places the ship
-ship_col = random_col(board) # places the ship
 
+# show board
+print_board(board)
+
+# place the ship at row
+ship_row = random_row(board)
+
+# place the ship at column
+ship_col = random_col(board)
+
+# create integer variable turn and initialize to 1
 turn = 1
+
+# loop if within 3 tries
 while turn < 4:
-    print("Turn", turn)
-    guess_row = int(input("Guess Row: "))-1 # gets row from user and converts to integer board index
-    guess_col = int(input("Guess Col: "))-1 # gets col from user and converts to integer board index
+    print("Turn", turn) # show try number
+    guess_row = int(input("Guess Row: ")) - 1 # convert input row to integer board index
+    guess_col = int(input("Guess Col: ")) - 1 # convert input column to integer board index
     # check hit
-    if guess_row == ship_row and guess_col == ship_col: # if the user has hit the ship
+    if guess_row == ship_row and guess_col == ship_col: # if user hit the ship
         print ("Congratulations! You sunk the battleship!")
         break # exit loop
     else:
         if (guess_row < 0 or guess_row > 2) or (guess_col < 0 or guess_col > 2): # out of range
             print("Oops, that's not even in the ocean.")
-        elif(board[guess_row][guess_col] == "X"): # the spot has already been hit
+        elif board[guess_row][guess_col] == "X": # spot has already been hit
             print("You guessed that one already.")
-        else: # the player hits an empty spot
+        else: # player hits empty spot
             print("You missed the battleship!")
             board[guess_row][guess_col] = "X" 
             turn = turn + 1
             print_board(board)
         if turn == 4: # after 3 tries (turn starts from 1)
-            print("Game Over")
-            print("The Battleship is in row", ship_row, ", col", ship_col)
+            print("Game over")
+            print("The battleship is in row", ship_row + 1, ", col", ship_col + 1)
